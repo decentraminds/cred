00fbf77c707d43966d54be4d88c0dba6a3579373
Santiago Gonzalez <hernangt12re3@gmail.com>
2020-03-23 00:05:32 -0500

    Fix error with Github Action

--- update.sh
+++ update.sh
@@ -9,11 +9,11 @@ cd $sc
 
 ./scripts/build_static_site.sh \
   --target "$PATH_TO_THIS_REPOSITORY/docs"  \
-  --project @decentraminds
-  # --project-file "$PATH_TO_THIS_REPOSITORY/project.json" #\
+  # --project @decentraminds
+  --project-file "$PATH_TO_THIS_REPOSITORY/project.json" #\
   # --weights "$PATH_TO_THIS_REPOSITORY/weights.json"
 
 # Load it twice so we can access the scores -- it's a hack, pending real instance system
-# node ./bin/sourcecred.js load --project "$PATH_TO_THIS_REPOSITORY/project.json" #--weights "$PATH_TO_THIS_REPOSITORY/weights.json"
-node ./bin/sourcecred.js load @decentraminds #--weights "$PATH_TO_THIS_REPOSITORY/weights.json"
+node ./bin/sourcecred.js load --project "$PATH_TO_THIS_REPOSITORY/project.json" #--weights "$PATH_TO_THIS_REPOSITORY/weights.json"
+# node ./bin/sourcecred.js load @decentraminds #--weights "$PATH_TO_THIS_REPOSITORY/weights.json"
 node ./bin/sourcecred.js scores @decentraminds | jq '.' > "$PATH_TO_THIS_REPOSITORY/scores.json"
